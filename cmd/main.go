b003c5f4be9bb4ce5db0c30886d03328f6211230
Stas773 <kornevkumer@mail.ru>
2023-05-17 21:10:10 +0500

    'changed requests'

--- cmd/main.go
+++ cmd/main.go
@@ -21,13 +21,13 @@ func main() {
 
 	userDB.UserDB = make(map[uuid.UUID]userDB.User)
 
-	router.Post("/create", createUser.CreateUser)
+	router.Post("/users", createUser.CreateUser)
 	router.Post("/make_friends", makeFriends.MakeFriends)
-	router.Get("/Get/{userId}", getUser.GetUser)
-	router.Get("/Get/all", getAllUsers.GetAllUsers)
-	router.Get("/Get/friends/{userId}", getUserFriends.GetUserFriends)
-	router.Patch("/Patch/{userId}", updateUser.UpdateUser)
-	router.Delete("/Delete/{userId}", deleteUser.DeleteUser)
+	router.Get("/users/{userId}", getUser.GetUser)
+	router.Get("/users", getAllUsers.GetAllUsers)
+	router.Get("/users/friends/{userId}", getUserFriends.GetUserFriends)
+	router.Patch("/users/{userId}", updateUser.UpdateUser)
+	router.Delete("/users/{userId}", deleteUser.DeleteUser)
 
 	http.ListenAndServe(":8080", router)
 }
